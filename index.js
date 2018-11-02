cba5e3c1a912e35692ef4a3e66a5eb7c48963122
Joseph Akayesi <josephakayesi@gmail.com>
2018-11-02 17:57:04 +0000

    Initial commit of my prototype boilerplate template. Toggle checkboxes to add or remove section(boxes)

---
+++ index.js
@@ -0,0 +1,54 @@
+
+function boxOneToggle() {
+    var x = document.getElementById("boxOne");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function boxTwoToggle() {
+    var x = document.getElementById("boxTwo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function boxThreeToggle() {
+    var x = document.getElementById("boxThree");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function boxFourToggle() {
+    var x = document.getElementById("boxFour");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function boxFiveToggle() {
+    var x = document.getElementById("boxFive");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function boxSixToggle() {
+    var x = document.getElementById("boxSix");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
